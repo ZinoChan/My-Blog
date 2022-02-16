ea6e1907530ada6829f2e3588b0142b1ba1dd981
Zineb ELGUIL <devzinochan@gmail.com>
2022-02-16 15:42:05 +0000

    add layout & fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import Layout from "../components/Layout";
+import "@fontsource/archivo-narrow";
+import "@fontsource/archivo-narrow/700.css";
+import "@fontsource/source-sans-pro";
+import "@fontsource/source-sans-pro/700.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />;
+    </Layout>
+  );
 }
 
 export default MyApp;
