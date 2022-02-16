ce15c65a96694b4bc083b3d33c766a58ed2a9a1c
Zineb ELGUIL <devzinochan@gmail.com>
2022-02-16 19:21:41 +0000

    add react-hamburger menu

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,21 @@
+import { Squeeze as Hamburger } from "hamburger-react";
+import { useState } from "react";
+
 const Layout = ({ children }) => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <main>
       <header>
         <nav className="max-w-screen-lg mx-auto p-2 flex items-center justify-between">
           <div>
-            <h1 className="text-xl uppercase text-primary-100">logo</h1>
+            <h1 className="text-xl font-primary font-bold uppercase text-primary-100">
+              logo
+            </h1>
           </div>
-          <div></div>
+          <div>
+            <Hamburger toggled={isOpen} toggle={setOpen} />
+          </div>
         </nav>
       </header>
       {children}
