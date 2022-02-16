daeea4a647f9b29314efbff56bc07643d36afe7b
Zineb ELGUIL <devzinochan@gmail.com>
2022-02-16 13:47:16 +0000

    add tailwind theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      primary: ["Archivo Narrow", ...defaultTheme.fontFamily.serif],
+      secondary: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      colors: {
+        "primary-100": "#2231AA",
+        "primary-200": "#3C48AA",
+        "primary-300": "#666DAA",
+        "primary-400": "#A4ADF8",
+        "primary-500": "#E1E4FF",
+        "secondary-100": "#2231AA",
+        "secondary-200": "#3C48AA",
+        "secondary-300": "#666DAA",
+        "secondary-400": "#FFD9C8",
+      },
+    },
   },
   plugins: [],
 };
