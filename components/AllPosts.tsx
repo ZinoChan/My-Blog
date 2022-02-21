ea107cc78ed8add63b2d6f8c5c7383b9686ef6e5
Zineb ELGUIL <devzinochan@gmail.com>
2022-02-21 19:26:14 +0000

    add responsive design to all post section

--- components/AllPosts.tsx
+++ components/AllPosts.tsx
@@ -3,11 +3,11 @@ import PostCard from "./PostCard";
 const AllPosts = ({ posts }) => {
   return (
     <section className="py-20">
-      <div className="max-w-screen-lg mx-auto">
+      <div className="max-w-screen-lg mx-auto px-2">
         <h2 className="mb-16 font-primary text-primary-dark-100 font-bold text-3xl text-center">
           All Articles
         </h2>
-        <div className="grid grid-cols-3 gap-8">
+        <div className="grid md:grid-cols-3 gap-8">
           {posts.map((post) => (
             <PostCard post={post} key={post.sys.id} />
           ))}
